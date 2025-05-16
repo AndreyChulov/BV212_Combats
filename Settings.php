fbcd540ce50fae9e6944a4b49583b25f621b4396
AndreyChulov <andreyc253@gmail.com>
2025-05-16 21:40:15 +0300

    Fix localhost variable

--- Settings.php
+++ Settings.php
@@ -3,5 +3,5 @@
 global $ROOT_URL;
 global $SESSION_TIMEOUT;
 
-$ROOT_URL = $_ENV["BV212_Combats_Site"] ?? "https://localhost";
+$ROOT_URL = $_ENV["BV212_Combats_Site"] ?? "http://localhost";
 $SESSION_TIMEOUT = 2*60;//2 minutes
