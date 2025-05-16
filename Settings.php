c7e698f6790dd11c8e6d5a27df765e63eaa3a455
AndreyChulov <andreyc253@gmail.com>
2025-05-16 21:27:19 +0300

    Pregect recreated. Settings updated

--- Settings.php
+++ Settings.php
@@ -3,5 +3,5 @@
 global $ROOT_URL;
 global $SESSION_TIMEOUT;
 
-$ROOT_URL = "https://bv212--combats-ezop.amvera.io";
+$ROOT_URL = "https://bv212-combats-ezop.amvera.io";
 $SESSION_TIMEOUT = 2*60;//2 minutes
