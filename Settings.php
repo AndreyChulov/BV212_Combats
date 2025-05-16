5e9e4169ff929bda99fd2dae08afb582806dae19
AndreyChulov <andreyc253@gmail.com>
2025-05-16 21:38:04 +0300

    Use environment variable

--- Settings.php
+++ Settings.php
@@ -3,5 +3,5 @@
 global $ROOT_URL;
 global $SESSION_TIMEOUT;
 
-$ROOT_URL = "https://bv212-combats-ezop.amvera.io";
+$ROOT_URL = $_ENV["BV212_Combats_Site"] ?? "https://localhost";
 $SESSION_TIMEOUT = 2*60;//2 minutes
